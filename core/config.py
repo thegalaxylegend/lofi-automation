8c073b461352461c911d6f0725f6c3f2e6d5663b
thegalaxylegend <thegalaxylegend@users.noreply.github.com>
2026-06-11 08:29:59 +0530

    fix: wire all 8 Groq keys, add HF tokens, fix scout secret leak

--- core/config.py
+++ core/config.py
@@ -149,7 +149,7 @@ class Config:
         logger.info("Environment probe found keys: %s", ", ".join(llm_keys_found) if llm_keys_found else "NONE")
 
         self.gemini_keys: list[str] = self._collect_keys("GEMINI_API_KEY_", 6)
-        self.groq_keys: list[str] = self._collect_keys("GROQ_API_KEY_", 5)
+        self.groq_keys: list[str] = self._collect_keys("GROQ_API_KEY_", 8)
 
         self.pexels_api_key: str = os.getenv("PEXELS_API_KEY", "")
         self.pixabay_api_key: str = os.getenv("PIXABAY_API_KEY", "")
